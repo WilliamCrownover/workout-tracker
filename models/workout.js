fac71a3af20307e8aa996109cc178f3b2685db62
William Crownover <williamcrownover1@gmail.com>
2021-09-12 14:41:07 -0700

    Finished workout model definitions

--- models/workout.js
+++ models/workout.js
@@ -12,8 +12,27 @@ const WorkoutSchema = new Schema({
                 type: String
             },
             name: {
-                
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
             }
         }
     ]
-})
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
