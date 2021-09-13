0474db215d7b83f1527757b153432f461ea8b7b1
William Crownover <williamcrownover1@gmail.com>
2021-09-12 23:16:32 -0700

    Setup route to stats page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,4 +1,9 @@
 // URL localhost:3000/
 const router = require( 'express' ).Router();
+const path = require( 'path' );
+
+router.get( '/stats', async ( req, res ) => {
+    res.sendFile( path.join( __dirname, '/../public/stats.html'));
+});
 
 module.exports = router;
