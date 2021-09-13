eaa86d421484ace5213ae9c4f23e5d22def02718
William Crownover <williamcrownover1@gmail.com>
2021-09-13 01:48:22 -0700

    Created api route to create new workout

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -43,4 +43,16 @@ router.put( '/:id', async ( req, res ) => {
     }
 });
 
+// Create a new workout
+router.post( '/', async ( req, res ) => {
+    try {
+        const workoutData = await db.Workout.create( req.body );
+
+        res.status( 200 ).json( workoutData );
+
+    } catch ( err ) {
+        res.status( 400 ).json( err );
+    }
+});
+
 module.exports = router;
