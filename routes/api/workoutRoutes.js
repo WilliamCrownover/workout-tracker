f673e7af6f6baef072a442f45140b90920016406
William Crownover <williamcrownover1@gmail.com>
2021-09-13 01:36:05 -0700

    Created api route to add exercises to a workout

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -2,6 +2,7 @@
 const router = require( 'express' ).Router();
 const db = require("../../models");
 
+// Get all the workout data and add a totalDuration field
 router.get( '/', async ( req, res ) => {
     try {
         const workoutsData = await db.Workout.aggregate([
@@ -15,6 +16,27 @@ router.get( '/', async ( req, res ) => {
         ]);
 
         res.status( 200 ).json( workoutsData );
+
+    } catch ( err ) {
+        res.status( 400 ).json( err );
+    }
+});
+
+// Push a new exercise to existing workout
+router.put( '/:id', async ( req, res ) => {
+    try {
+        const workoutData = await db.Workout.updateOne(
+            {
+                _id: req.params.id
+            },
+            {
+                $push: {
+                    exercises: {...req.body}
+                }
+            }
+        );
+
+        res.status( 200 ).json( workoutData );
 
     } catch ( err ) {
         res.status( 400 ).json( err );
